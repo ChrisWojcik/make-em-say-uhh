df55cf5e92ca1b84584af61c54f143085ca75136
Hector Leiva <hector@hectorleiva.com>
2022-01-26 09:34:54 -0500

    adding TS support for svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,8 @@ const config = {
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+	preprocess: preprocess()
 };
 
 export default config;
